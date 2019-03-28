eabad8bddea716c784bef78f648dc3fc1949e807
tmohammad78 <mohammadtaheri506@gmail.com>
2019-03-27 23:40:12 -0700

    creat controller

--- resturant/src/js/index.js
+++ resturant/src/js/index.js
@@ -1,12 +1,30 @@
-// import string from './models/Search';
-// // import { add,multiply,ID } from './views/searchView';
-// import * as searchView from './views/searchView';
-// console.log(`this is a example ${searchView.add(searchView.ID,2)} and ${searchView.multiply(3,5)}.${string}`)
+import Search from './models/Search';
+const state={};//Like prototype ??
+const controlsearch = async ()=>{
+    //1)get query from view
+    const query='pizza';
+    if(query){
+        //2)new search and add to state 
+        state.search=new Search(query);
+        //3) prepare UI for result
+        //4)search for recipe
+        await state.search.getResult();
+        //5)render result
+        console.log(state.search.result);
+    }
+}
+document.querySelector('.search').addEventListener('submit',e=>{
+    e.preventDefault();
+    controlsearch();
+});
+
+
+
+
+
+
+
 
 
 // 36833e2f2d29f546b4599c757b1394c4
 //https://www.food2fork.com/api/search
-import Search from './models/Search';
-const search=new Search('pizza');
-console.log(search);
-search.getResult();
--- resturant/src/js/models/Search.js
+++ resturant/src/js/models/Search.js
@@ -9,7 +9,7 @@ export default class Search{
         try {
             const res=await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result=res.data.recipes;
-            console.log(this.result);
+            // console.log(this.result);
         } catch (error) {
             alert(error);
         }
