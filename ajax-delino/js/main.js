0320a1911fd6164ac1d210a7f8caf1d3faab9f96
tmohammad78 <mohammadtaheri506@gmail.com>
2019-04-06 14:32:32 +0430

    render for category and id equal 0

--- ajax-delino/js/main.js
+++ ajax-delino/js/main.js
@@ -1,197 +1,180 @@
-$(document).ready(function(){
-    const $categorywrapper=$('.categories');
-    const $wrapper=$('.food_section-info');
+$(document).ready(function () {
+    const $categorywrapper = $('.categories');
+
+
+
+
+    const $wrapper = $('.food_section-info');
+    const $test = $('.test');
     // const $wrapper=$('');
     let delinodata;
     // const key='2a54ea59-76ad-4f8b-9856-1a7bdbc22c4c';
-    const key='48ff20a8-c1a4-4843-8826-ae0ba77f4254';
+    //const key = '48ff20a8-c1a4-4843-8826-ae0ba77f4254';
     //a5fa43c3-234d-462a-990a-9ec7ed82159f
+    const key = "890d958f-9e64-4211-a2fa-d732c7a3920f" // https://www.delino.com/restaurant/toasthouse
 
-    $.get(`https://api.delino.com/restaurant/menu/${key}`).done(result=>{
-            delinodata=result.categories;
-            rendercategory(delinodata);
-            console.log(delinodata);
-            for (var i=0 ; i< delinodata.length ; i++){
-                renderfood(delinodata[i]);
-            }
-        }).fail((xhr)=>{
-        if(xhr.status==404)
-        {
+    $.get(`https://api.delino.com/restaurant/menu/${key}`).done(result => {
+        delinodata = result.categories;
+        rendercategory(delinodata);
+        console.log(delinodata);
+        for (var i = 0; i < delinodata.length; i++) {
+            renderfood(delinodata[i]);
+        }
+    }).fail((xhr) => {
+        if (xhr.status == 404) {
             alert('not found');
         }
     });
-    function rendercategory(data){
-        let html="";
-        if(data){
-            data.sort(function(a, b){return a.index-b.index});
+    function rendercategory(data) {
+        let html = "";
+        if (data) {
+            data.sort(function (a, b) { return a.index - b.index });
 
-            console.log(data);            
-            const itemmenue=data.map((item,i)=>{
+            console.log(data);
+            const itemmenue = data.map((item, i) => {
+                // <a href="" class="clearfix scroll" data-cat-id="${item.id}" >
+                // </a>
                 return `
-                <div class="categories__indexbox" data-cat-id="${item.id}" >
-                    <span class="categories__span clearfix">
-                        <a id='' href="#${item.id}"><img class="categories__img " src="img/${item.logo}.png" alt="${item.title}"></a>
+                    <div class="categories__indexbox " data-cat-id="${item.id}" >
+                            <a id='' href="#${item.id}"><img class="categories__img " src="img/${item.logo}.png" alt="${item.title}"></a>
                         <b class="categories__caption">${item.title}</b>
-                    </span>
-                </div>
+                    </div>
                 `;
             });
-            html=itemmenue.join("");
+            html = itemmenue.join("");
         }
         $categorywrapper.html(html);
     }
-    function renderfood(data){
+    function renderfood(data) {
         // console.log(data)
         // console.log(data.sub[0]);
-        for (var i=0;i<data.sub.length;i++){
-        const subdata=data.sub[i].food;
-        if(subdata){
-            const itemsBox = subdata.map((item,i)=>{
-                console.log(helper.truncate(item.ingredient));
-                const image =  item.img ? '<img  class="food_section-category__img" src="' + item.img.replace("#SIZEOFIMAGE#", "280x175") +'"/>' : "";
-                return `
-                <a href=#${item.id} >
-                <div class="food_section-category col-1-of-3">
-                <div class="food_section-img">
-                     ${image}    
+
+        let html = '<h1 style="font-size: 20px; text-align: center">' + data.title + '</h1>';
+
+        for (var i = 0; i < data.sub.length; i++) {
+
+            //debugger
+
+            if (data.sub[i].id == 0) {
+
+                const subdata = data.sub[i].food;
+                if (subdata) {
+                    const itemsBox = subdata.map((item, i) => {
+                        //console.log(helper.truncate(item.ingredient));
+                        let image = item.img ? '<img  class="food_section-category__img" src="' + item.img.replace("#SIZEOFIMAGE#", "280x175") + '"/>' : "";
+                        return `
+                    <div class="food_section-category col-1-of-3" data-food='${JSON.stringify(item)}'>
+                        <div class="food_section-img">
+                            ${image}    
+                        </div>
+                        <div class="food_section-description">
+                            <div class="food_section-description-index">
+                                <h3 class="food_section-description-title">${item.title}</h3>
+                                <span >${helper.truncate(item.ingredient)}</span>
+                            </div>
+                        </div>
+                        <div class="food_section-last clearfix">
+                            <div class="food_section-last-price">
+                                ${helper.currancy(item.price)}
+                            </div>
+                            <div class="food_section-last-cart">
+                                <a class="food_section-last-carticon" href=""><span >
+                                <i class="fas fa-shopping-cart"></i></span></a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                    });
+                    html += ` <div style="border: 1px solid #eee" class="food_section-infobox" id="box-${data.id}">` + itemsBox.join("") + `</div>`;
+
+                }
+            } else {
+
+                let image = data.sub[i].img ? '<img  class="food_section-category__img" src="' + data.sub[i].img.replace("#SIZEOFIMAGE#", "280x175") + '"/>' : "";
+                html += `
+                <div class="food_section-category col-1-of-3" data-food-id="${data.sub[i].id}">
+                    <div class="food_section-img">
+                        ${image}    
+                    </div>
+                    <div class="food_section-description">
+                        <div class="food_section-description-index">
+                            <h3 class="food_section-description-title">${data.sub[i].title}</h3>
+                            <span >${helper.truncate(data.sub[i].priceLabel)}</span>
+                        </div>
+                    </div>
+                    <div class="food_section-last clearfix">
+                        <div class="food_section-last-price">
+                            ${helper.currancy(data.sub[i].priceLabel || 0)}
+                        </div>
+                        <div class="food_section-last-cart">
+                            <a class="food_section-last-carticon" href=""><span >
+                            <i class="fas fa-shopping-cart"></i></span></a>
+                        </div>
+                    </div>
                 </div>
-                <div class="food_section-description">
-                     <div class="food_section-description-index">
-                          <h3 class="food_section-description-title">${item.title}</h3>
-                          <span >${helper.truncate(item.ingredient)}</span>
-                     </div>
-                </div>
-                <div class="food_section-last clearfix">
-                      <div class="food_section-last-price">
-                          ${helper.currancy(item.price)}
-                      </div>
-                      <div class="food_section-last-cart">
-                          <a class="food_section-last-carticon" href=""><span >
-                          <i class="fas fa-shopping-cart"></i></span></a>
-                      </div>
-                </div>
-          </div>
-          </a>
             `;
-            });
-            html=` <div style="border: 1px solid #eee" class="food_section-infobox" id="box-${data.id}">`+itemsBox.join("")+`</div>`;
-            $wrapper.append(html);
-        }
-        }
-    }
-    //popup
-    // $wrapper.on("click","a",function(data){
-    //     // for(var i=0 ;i<data.sub.length;i++){
-    //     //     // renderpopup(delinodata[i].sub);
-    //     //     console.log(data.sub);
-
-    //     // }
-    //     // // console.log(delinodata);
-    //     // return;
-    //     // renderpopup(data);
-    //     controlpop();
-    //     getfood();
-
-        
-    // });
-    const renderpop=function(data){
-        for (var i=0;i<data.sub.length;i++){
-            const subdata=data.sub[i].food;
-            // subdata.indexOf(10035);
-            let arr=[];
-
-            $items=subdata.map((item,i)=>{
-                arr.push(item.id);
-            });
-            console.log(arr.find(function(element,number=10035){
-                if(number==element) return true;
-            }));
-            // if(subdata){
-            //     $test1=$wtf.map((item,i)=>{
-            //         const $test=item.id.toString().indexOf(10035);
-            //         console.log($wtf[$test]);
-            //     })
-            // }
-        }
-    }
-    const getfood=function(delinodata){
-        // this.id=id;
-            // this.title=getting.categories
-            // this.title=result.categories[0].sub[0].food;
-            // $wtf=data.categories;
-            // console.log($wtf);
-            for (var i=0 ; i< delinodata.length ; i++){
-                renderpop(delinodata[i]);
             }
-
-            
-
-            // console.log($wtf[$test]);
-
-            // for (var i=0 ; i< delinodata.length ; i++){
-            //     renderfood(delinodata[i]);
-            // }
-      
-    }
-    const controlpop=function(){
-    $.get(`https://api.delino.com/restaurant/menu/${key}`).done(result=>{
-        delinodata=result.categories;
-            }).fail((xhr)=>{
-            if(xhr.status==404)
-            {
-                alert('not found');
-            }
-        });
-        const id=window.location.hash.replace('#','');
-        console.log(id);
-        if(id){
-            getfood(delinodata);
         }
+
+        $wrapper.append(html);
     }
-    // function renderpopup(data){
-    //     let html= "";
-    //     if(data){
-    //         alert('s');
-    //         $('.test').addClass('popup',function(){
-    //             // console.log('works');
-    //             return ' ';
-    //         });
-    //         // const itempoup=data.map((item,id)=>{
-    //         //     return `
-    //         //     <div class="popup__content">
-    //         //         <a href="" class="popup__close" >&times;</a>
-    //         //         <h3 class="popup_description">${item.ingredient} </h3>
-    //         //     </div>
-    //         //     `;
-    //         // });
-    //         // html=itempoup.join("");            
-    //     }
-    //     // $("body").html(html);
-    // }
-    $('.popup__close').on("click",function(){
-        $('.test').fadeOute(2000,function(){    });
-    });
+    function renderpopup(data) {
+        console.log(data);
+        $('.test').addClass('popup').html(`
+           <div class="popup__content">
+                <i class="fas fa-times popup__content-close"></i>
+                <div class="popup__content-img">
+                    <img src="img/pizza3.jpg" alt="">
+                </div>
+                <div class="popup__content-text">
+                   sfgehws
+                </div>
+ 
+            </div>
+           `);
+    }
+
     //popup
-    $categorywrapper.on("click","a",function(){
+    $categorywrapper.on("click", "a", function () {
         //debugger;
         const $box = $("#box-" + $(this).closest("div").data("cat-id"));
-        if ($box.length){
+        if ($box.length) {
             $('body').stop().animate({
                 scrollTop: $box.offset().top - 90
-              }, 600 );
-        }            
+            }, 600);
+        }
     });
-    window.addEventListener('hashchange',controlpop);
+    $test.on("click", "i", function () {
+        $('.test').removeClass("popup")//.fadeOut(.5);
+    })
+    $wrapper.on("click", "img", function () {
+
+        const itemData = $(this).closest(".food_section-category").data("food")
+
+        console.log(itemData.id)
+        //const data = id//$(this)
+        //renderpopup(data);
+    });
+
+
+
+
+
+    //scrollwatcher
+
+
     // $(window).scroll(function(){
-    //     var scrolldefault=$(window).scrollTop;
-        
-    //     const $box = $("#box-" + $(this).closest("div").data("cat-id"));
-    //     var scrolllocation=$box.offset().top -20;
-    //     if()
-    //     $('').
+    //     var scrollposition=$(window).scrollTop(); 
 
+    //     const $box = $("#box-"+$('.scroll').data("cat-id"));
+    //     $('.scroll').each(function(){
+    //         var scrollafter=$box.offset().top-20;
+    //         if ( scrollafter <= scrollposition ) {
+    //             $(this).addClass('active');
+    //             $(this).siblings().removeClass('active');
+    //           }
+    //     });
     // });
-
 });
 
 
