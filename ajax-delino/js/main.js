4e5235a2f6abd41d16813ee5c672aa875c9505f6
tmohammad78 <mohammadtaheri506@gmail.com>
2019-04-15 14:23:28 +0430

    fix add food

--- ajax-delino/js/main.js
+++ ajax-delino/js/main.js
@@ -3,18 +3,17 @@ $(document).ready(function() {
   const $wrapper = $(".food_section-info");
   const $modal = $(".modal");
   let delinoData,
-    cart=[],
+    cart = [],
     itemsTop = [],
     scrolling = false;
-  let number = 0;
-  //let cart = {};
+  let objectCart = {};
   // const key='2a54ea59-76ad-4f8b-9856-1a7bdbc22c4c';
   //const key = '48ff20a8-c1a4-4843-8826-ae0ba77f4254';
   //a5fa43c3-234d-462a-990a-9ec7ed82159f
   const key = "890d958f-9e64-4211-a2fa-d732c7a3920f"; // https://www.delino.com/restaurant/toasthouse
   const tempCart = sessionStorage.getItem("cart");
   console.log(tempCart);
-
+console.log(cart);
   $.get(`https://api.delino.com/restaurant/menu/${key}`)
     .done(result => {
       delinoData = result.categories;
@@ -28,9 +27,11 @@ $(document).ready(function() {
         itemsTop.push(parseInt(top));
       });
       $('button[data-buy="buyfood"]').on("click", function() {
-        $(this).closest(".food_section-last-cart").css({
+        $(this)
+          .closest(".food_section-last-cart")
+          .css({
             padding: "0"
-        });
+          });
         renderCart($(this));
       });
       console.log(itemsTop);
@@ -40,8 +41,8 @@ $(document).ready(function() {
         alert("not found");
       }
     });
-//start rendering category
-function renderCategory(data) {
+  //start rendering category
+  function renderCategory(data) {
     let html = "";
     if (data) {
       data.sort(function(a, b) {
@@ -59,218 +60,146 @@ function renderCategory(data) {
       html = itemMenue.join("");
     }
     $categoryWrapper.html(html);
-}
-//End rendering category
+  }
+  //End rendering category
 
-// start rendering food card 
-function renderFood(data){
-    let html = '<h1 style="font-size: 20px; text-align: center">' + data.title + "</h1>";
+  // start rendering food card
+  function renderFood(data) {
+    let html =
+      '<h1 style="font-size: 20px; text-align: center">' + data.title + "</h1>";
     for (var i = 0; i < data.sub.length; i++) {
       if (data.sub[i].id == 0) {
-        //const id = data.sub[i].id;
+        let id = data.sub[i].id;
+        console.log(data.sub[i].id);
         const subData = data.sub[i].food;
         if (subData) {
           // updateView(id);
-          const tpl_Food= tmpl($("#template-food").html());
+          const tpl_Food = tmpl($("#template-food").html());
           const itemsBox = subData.map((item, i) => {
-            const quantity = cart[item.id] || 0;
-            const image = item.img ? '<img  class="food_section-category__img" src="' + item.img.replace("#SIZEOFIMAGE#", "280x175") + '"/>': "";
+            // const quantity = cart[item.id] || 0;
+            const image = item.img
+              ? '<img  class="food_section-category__img" src="' +
+                item.img.replace("#SIZEOFIMAGE#", "280x175") +
+                '"/>'
+              : "";
             return tpl_Food({
-              items:JSON.stringify(item),
-              image:image,
-              title:item.title,
-              truncate:helper.truncate(item.ingredient),
-              curancy:helper.currancy(item.price),
-              quantity:quantity
-            })
+              items: JSON.stringify(item),
+              image: image,
+              title: item.title,
+              truncate: helper.truncate(item.ingredient),
+              curancy: helper.currancy(item.price),
+              quantity: cart[0] || ''
+            });
           });
-          html +=` <div style="border: 1px solid #eee" class="food_section-infobox" id="box-${data.id }">` + itemsBox.join("") + `</div>`;
+          html +=
+            ` <div style="border: 1px solid #eee" class="food_section-infobox" id="box-${
+              data.id
+            }">` +
+            itemsBox.join("") +
+            `</div>`;
         }
-      }else{
-        const tpl_foodElse= tmpl($("#template-foodElse").html());
-        let image = data.sub[i].img ? '<img  class="food_section-category__img" src="' + data.sub[i].img.replace("#SIZEOFIMAGE#", "280x175") + '"/>' : "";
+      } else {
+        const tpl_foodElse = tmpl($("#template-foodElse").html());
+        let image = data.sub[i].img
+          ? '<img  class="food_section-category__img" src="' +
+            data.sub[i].img.replace("#SIZEOFIMAGE#", "280x175") +
+            '"/>'
+          : "";
         const id = data.sub[i].id;
-        const quantity = '';
-        const itemFood=tpl_foodElse({
-          id:id,
-          image:image,
-          title:data.sub[i].title,
-          priceLabel:helper.truncate(data.sub[i].priceLabel),
-          currancy:helper.currancy(data.sub[i].priceLabel || 0),
-          quantity:  0//quantity
-        })
-        html +=itemFood;
+        const quantity = "";
+        const itemFood = tpl_foodElse({
+          id: id,
+          image: image,
+          title: data.sub[i].title,
+          priceLabel: helper.truncate(data.sub[i].priceLabel),
+          currancy: helper.currancy(data.sub[i].priceLabel || 0),
+          quantity: 0 //quantity
+        });
+        html += itemFood;
       }
     }
     $wrapper.append(html);
-}
-//End rendering food card
+  }
+  //End rendering food card
 
-//start rendering popup
-function renderPopup(data) {
+  //start rendering popup
+  function renderPopup(data) {
     console.log(data);
-    id = data.id;
+    let id = data.id;
     console.log(id);
-    let image = data.img ? '<img  class="popup__content-img--inner"" src="' + data.img.replace("#SIZEOFIMAGE#", "560x350") + '"/>': ""; //560×350
-    const tpl_popup= tmpl($("#template-popup").html());
-    //updateView(id);
+    let image = data.img
+      ? '<img  class="imageFood"" src="' +
+        data.img.replace("#SIZEOFIMAGE#", "560x350") +
+        '"/>'
+      : ""; //560×350
+    const tpl_popup = tmpl($("#template-popup").html());
     //Add to html
-    const itempopup=tpl_popup({
+    const itempopup = tpl_popup({
       id: id,
-      image:image,
-      title:data.title,
-      ingredient:data.ingredient,
-      currancy:helper.currancy(data.price),
+      image: image,
+      title: data.title,
+      ingredient: data.ingredient,
+      currancy: helper.currancy(data.price),
       quantity: cart[id] || 0
-    })
-    $modal.addClass("popup").html(itempopup);
-    // let $modaling = $(".popup__content-btn").data("modal-button");
-    // console.log("test", $modaling);
-    // if ($modaling === false) {
-    //   $(".popup__content-btn").css({
-    //     background: "#d2d2d2",
-    //     cursor: "default"
-    //   });
-    //   $modaling = $(".popup__content-btn").attr("data-modal-button", "true");
-    // }
-    $modal.on("click",'div[data-buy="buyfood"]',function(e){
-      e.preventDefault();
-      const tpl_countFood= tmpl($("#template-countFood").html());
-        const counting=tpl_countFood({
-          ////update
-          number:number
-        });
-       $(".popup__content-addbtn").addClass("countcart").html(counting);
-
     });
-}
-//End render popup
-// function updateView(id){
-//   if (tempCart){
-//      cart = JSON.parse(tempCart) || [];
-//      console.log(cart);
-//       if(cart[id]){
-//         number=cart[id];
-//       }else{
-//           number=0;
-//       }
-//   }
-// }
+    $modal.addClass("popup").html(itempopup);
+  }
+  //End render popup
 
-$modal.on("click",".cart-action", function() {
-  const $id=$(this).closest('.popup__content').data("popup");  
-  calcute("add",$id);
-});
-$modal.on("click",".minus_btn",function(){
-  calcute("minus",$id);
-});
+  $modal.on("click", ".quantity-holder button", e => {
+    e.preventDefault();
+    const $btn = $(e.target).closest("button");
+    const $holder = $btn.parent()
+    const id = $btn.closest(".popup__content").data("food-id");
+    let qty = id ? cart[parseInt(id)] || 0 :  0;
+    //debugger
+    switch ($btn.data("cmd")) {
+      case "increase":
+        qty++;
+        $holder.addClass("selected") ;
+        break;
+      case "decrease":
+        qty--;
+        if (qty<1){
+          console.log(true);
+          $(".popup__content").removeClass("selected");
+          $holder.removeClass("selected") ;
+        }
+        break;
+    }
+    cart[id] = qty;
+    $holder.find(".quantity").text(qty);
+  });
 
-function calcute(type,id){
+  // //Add count of food by clicking on btn-plus
+  // $modal.on("click", ".btn-plus", function() {
+  //   const id = $(this)
+  //     .closest(".popup__content")
+  //     .data("data-food-id");
+  //   calcute("add", id);
+  // });
 
+  // //Minus count of food by clicking on btn-minus
+  // $modal.on("click", ".btn-minus", function() {
+  //   calcute("minus", id);
+  // });
 
-
-  const operation=type === "add" ? number+=1:number-=1 ;
-  cart[id]=number; 
-
-  sessionStorage.setItem("cart",JSON.stringify(cart));
-  // console.log();
-}
-
-function storeCart(){
-
-}
-
-
-
-
-
-
-// $modal.on("click", 'div[data-buy="buyfood"]', function(e) {
-//     e.preventDefault();
-//     const tpl_countFood= tmpl($("#template-countFood").html());
-//     const counting=tpl_countFood({
-//       number:number
-//     });
-//     $(".popup__content-addbtn").addClass("countcart").html(counting);
-//     function check(type) {
-//       const situation = type === "add" ? (number += 1) : (number -= 1);
-//       if (type === "add") {
-//         count[$id] = number;
-//       } else {
-//         count[$id] = number;
-//       }
-//       console.log(count);
-//       // updateUI(count);
-//     }
-//     function updateUI(...count) {
-//       //console.log (Object.keys());
-//       console.log(count);
-//     }
-//     $(".add_btn").on("click", function() {
-//       check("add");
-//     });
-//     $(".minus_btn").on("click", function() {
-//       if (number > 1) {
-//         check("minus");
-//       } else if (number == 1) {
-//         $(".popup__content-btn").attr("data-modal-button", "false");
-//       }
-//     });
-//     // else if(number == 1){
-//     //   $('.popup__content-addbtn').removeClass('countcart');
-//     // }
-
-//     // rendercart('.popup__content-addbtn',data.id);
-//     // sessionStorage.setItem(data.id,number);
-//     $(".popup__content-btn").css({
-//       background: "linear-gradient(-60deg, #ef4123, #ef2379)",
-//       cursor: "pointer"
-//     });
-//     // for(var i=0; i<number;i++){
-//     //   console.log(data);
-//     // }
-
-//     // const situation1 =sessionStorage.getItem(data.id);
-
-//     // alert(situation1);
-//   });
-  
-
-  // function rendercart(classname,number){
-  //     $(classname).addClass('countcart').html(`
-  //     <button class="minus_btn" data-cmd="delete" ><i class="fas fa-minus"></i></button>
-  //     <span class="count-span" >${number}</span>
-  //     <button class="add_btn" data-cmd="add"><i class="fas fa-plus"></i></button>
-  //     `);
-  //     function check(type){
-  //       const situation= type === 'add' ? number += 1 : number -= 1;
-  //       count.push($(this).number);
-  //      console.log(count);
-  //       updateUI(situation);
-  //     }
-  //     function updateUI(situation){
-  //       $('.count-span').text()==situation;
-  //     }
-  //   $('.add_btn').on("click",function(){
-  //       check('add');
-  //   });
-  //   $('.minus_btn').on("click",function(){
-  //     if(number>1){
-  //       check('minus');
-  //     }else if(number==1){
-  //       $('.popup__content-btn').attr("data-modal-button","false");
-  //     }
-  //   });
-  //   // else if(number == 1){
-  //   //   $('.popup__content-addbtn').removeClass('countcart');
-  //   // }
+  // //calcute for add or minus count of foood
+  // function calcute(type, id) {
+  //   const operation = type === "add" ? (number += 1) : (number -= 1);
+  //   objectCart[id] = number;
+  //   sessionStorage.setItem("cart", JSON.stringify(objectCart));
+  //   // console.log();
   // }
+
   //popup
   $categoryWrapper.on("click", "a", function(e) {
     e.preventDefault();
     //e.stopPropagation() baes mishe ke event ma edame peida nakone va avalin sath ro begire
     scrolling = true;
-    const catId = $(this).parent().data("cat-id");
+    const catId = $(this)
+      .parent()
+      .data("cat-id");
     actievCategory(catId);
     let itemTopIndex = 0;
     for (var i = 0; i < delinoData.length; i++) {
@@ -279,21 +208,39 @@ function storeCart(){
         break;
       }
     }
-    $("body").stop().animate(
+    $("body")
+      .stop()
+      .animate(
         {
           scrollTop: itemsTop[itemTopIndex] - 100
-        },600);
+        },
+        600
+      );
     setTimeout(() => {
       scrolling = false;
     }, 600);
   });
-  $modal.on("click", ".popup__content-close", function() {
+
+  //close popuo
+  $modal.on("click", ".anc-close", function() {
     $(".modal").removeClass("popup");
   });
-  $wrapper.on("click", "img", function(){
-    const itemData = $(this).closest(".food_section-category").data("food");
+
+  //get id of parent for render popup
+  $wrapper.on("click", "img", function() {
+    const itemData = $(this)
+      .closest(".food_section-category")
+      .data("food");
     renderPopup(itemData);
   });
+
+  //for add class after click add btn
+  $modal.on("click", ".btn-plus", function() {
+    $(this)
+      .closest(".popup__content")
+      .addClass("selected");
+  });
+
   //scrollwatcher
   $(window).scroll(function() {
     if (!scrolling) {
@@ -301,7 +248,7 @@ function storeCart(){
       let activeBoxIndex = 0;
       for (var i = 0; i < itemsTop.length; i++) {
         if (scrollposition < itemsTop[i]) {
-           activeBoxIndex = i;
+          activeBoxIndex = i;
           break;
         }
       }
@@ -311,54 +258,36 @@ function storeCart(){
     }
   });
   function actievCategory(id) {
-    $(".categories").find('div[data-cat-id="' + id + '"]').addClass("active-box").siblings().removeClass("active-box");
+    $(".categories")
+      .find('div[data-cat-id="' + id + '"]')
+      .addClass("active-box")
+      .siblings()
+      .removeClass("active-box");
   }
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // const $box = $(
-    //   "#box-" +
-    //     $(this)
-    //       .closest("div")
-    //       .data("cat-id")
-    // );
-    // $(this)
-    //   .closest("div")
-    //   .addClass("active-box");
-    // $(this)
-    //   .closest("div")
-    //   .siblings()
-    //   .removeClass("active-box");
-    // if ($box.length) {
-    //   $("body")
-    //     .stop()
-    //     .animate(
-    //       {
-    //         scrollTop: $box.offset().top - 100
-    //       },
-    //       600
-    //     );
-    // }
+// const $box = $(
+//   "#box-" +
+//     $(this)
+//       .closest("div")
+//       .data("cat-id")
+// );
+// $(this)
+//   .closest("div")
+//   .addClass("active-box");
+// $(this)
+//   .closest("div")
+//   .siblings()
+//   .removeClass("active-box");
+// if ($box.length) {
+//   $("body")
+//     .stop()
+//     .animate(
+//       {
+//         scrollTop: $box.offset().top - 100
+//       },
+//       600
+//     );
+// }
