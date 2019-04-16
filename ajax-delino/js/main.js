d3708e88755072d64c56ff8a746e48ac0949fa8c
tmohammad78 <mohammadtaheri506@gmail.com>
2019-04-16 17:26:36 +0430

    edit function for cart

--- ajax-delino/js/main.js
+++ ajax-delino/js/main.js
@@ -3,52 +3,49 @@ $(document).ready(function() {
   const $wrapper = $(".food_section-info");
   const $modal = $(".modal");
   let delinoData,
-    cart = [],
-    itemsTop = [],
-    scrolling = false;
-  let objectCart = {};
+    CART_NAME = "cart";
+  (cart = {}), (itemsTop = []), (scrolling = false);
   // const key='2a54ea59-76ad-4f8b-9856-1a7bdbc22c4c';
   //const key = '48ff20a8-c1a4-4843-8826-ae0ba77f4254';
   //a5fa43c3-234d-462a-990a-9ec7ed82159f
   const key = "890d958f-9e64-4211-a2fa-d732c7a3920f"; // https://www.delino.com/restaurant/toasthouse
-  const tempCart = sessionStorage.getItem("cart");
-  console.log(tempCart);
-console.log(cart);
+  const tempCart = sessionStorage.getItem(CART_NAME);
+  if (tempCart) {
+    let cart_arr = tempCart.split("-");
+    cart_arr.forEach(node => {
+      let item = node.split(":");
+      cart[item[0]] = item[1];
+    });
+  }
+
   $.get(`https://api.delino.com/restaurant/menu/${key}`)
     .done(result => {
       delinoData = result.categories;
       renderCategory(delinoData);
-      console.log(delinoData);
       for (var i = 0; i < delinoData.length; i++) {
         renderFood(delinoData[i]);
       }
+      updateCart();
       $wrapper.find("h1").each((i, h1) => {
         const top = $(h1).offset().top;
         itemsTop.push(parseInt(top));
       });
-      $('button[data-buy="buyfood"]').on("click", function() {
-        $(this)
-          .closest(".food_section-last-cart")
-          .css({
-            padding: "0"
-          });
-        renderCart($(this));
-      });
-      console.log(itemsTop);
+
     })
     .fail(xhr => {
       if (xhr.status == 404) {
         alert("not found");
       }
     });
-  //start rendering category
+
+
   function renderCategory(data) {
     let html = "";
     if (data) {
       data.sort(function(a, b) {
         return a.index - b.index;
       });
-      console.log(data);
+      //console.log(data);
       const tpl_catItem = tmpl($("#template-category").html());
       const itemMenue = data.map((item, i) => {
         return tpl_catItem({
@@ -61,9 +58,7 @@ console.log(cart);
     }
     $categoryWrapper.html(html);
   }
-  //End rendering category
 
-  // start rendering food card
   function renderFood(data) {
     let html =
       '<h1 style="font-size: 20px; text-align: center">' + data.title + "</h1>";
@@ -79,13 +74,14 @@ console.log(cart);
                 item.img.replace("#SIZEOFIMAGE#", "280x175") +
                 '"/>'
               : "";
+
             return tpl_Food({
               items: JSON.stringify(item),
               image: image,
               title: item.title,
               ingredient: helper.truncate(item.ingredient),
               price: helper.currancy(item.price),
-              quantity: cart[id] || ''
+              quantity: cart[item.id] || ""
             });
           });
           html +=
@@ -117,132 +113,147 @@ console.log(cart);
     }
     $wrapper.append(html);
   }
-  //End rendering food card
+
+
 
   //start rendering popup
-function renderPopup(data) {
-  console.log(data);
-  let id = data.id;
-  console.log(id);
-  let image = data.img
-    ? '<img  class="imageFood"" src="' +
-      data.img.replace("#SIZEOFIMAGE#", "560x350") +
-      '"/>'
-    : ""; //560×350
-  const tpl_popup = tmpl($("#template-popup").html());
-  //Add to html
-  const itempopup = tpl_popup({
-    id: id,
-    image: image,
-    title: data.title,
-    ingredient: data.ingredient,
-    currancy: helper.currancy(data.price),
-    quantity: cart[id] || 0
-  });
-  $modal.addClass("popup").html(itempopup);
-}
+  function renderPopup(data) {
+    //console.log(data);
+    let id = data.id;
+    //console.log(id);
+    let image = data.img
+      ? '<img  class="imageFood"" src="' +
+        data.img.replace("#SIZEOFIMAGE#", "560x350") +
+        '"/>'
+      : ""; //560×350
+    const tpl_popup = tmpl($("#template-popup").html());
+    //Add to html
+    const itempopup = tpl_popup({
+      id: id,
+      image: image,
+      title: data.title,
+      ingredient: data.ingredient,
+      currancy: helper.currancy(data.price),
+      quantity: cart[id] || 0
+    });
+    $modal.addClass("popup").html(itempopup);
+  }
   //End render popup
 
-$modal.on("click", ".quantity-holder button", e => {
-  e.preventDefault();
-  const $btn = $(e.target).closest("button");
-  const $holder = $btn.parent()
-  const id = $btn.closest(".popup__content").data("food-id");
-  let qty = id ? cart[parseInt(id)] || 0 :  0;
-  //debugger
-  switch ($btn.data("cmd")) {
-    case "increase":
-      qty++;
-      $holder.addClass("selected") ;
-      break;
-    case "decrease":
-      qty--;
-      if (qty<1){
-        console.log(true);
-        $(".popup__content").removeClass("selected");
-        $holder.removeClass("selected") ;
+  $modal.on("click", ".quantity-holder button", e => {
+    e.preventDefault();
+    const $btn = $(e.target).closest("button");
+    const $holder = $btn.parent();
+    const id = $btn.closest(".popup__content").data("food-id");
+    let qty = id ? cart[parseInt(id)] || 0 : 0;
+    //debugger
+    switch ($btn.data("cmd")) {
+      case "increase":
+        qty++;
+        $holder.addClass("selected");
+        break;
+      case "decrease":
+        qty--;
+        if (qty < 1) {
+          //console.log(true);
+          $(".popup__content").removeClass("selected");
+          $holder.removeClass("selected");
+        }
+        break;
+    }
+    cart[id] = qty;
+    $holder.find(".quantity").text(qty);
+
+    updateCart();
+  });
+
+  //its for show count by click btn in card
+  $wrapper.on("click", ".quantity-holder button", e => {
+    e.preventDefault();
+    const $btn = $(e.target).closest("button");
+    const $holder = $btn.parent();
+    const id = $btn.closest(".food_section-category").data("food").id;
+    const title = $btn.closest(".food_section-category").data("food").title;
+    const price = $btn.closest(".food_section-category").data("food").price;
+    let qty = id ? cart[parseInt(id)] || 0 : 0;
+    switch ($btn.data("cmd")) {
+      case "increase":
+        qty++;
+        $holder.addClass("selected");
+        break;
+      case "decrease":
+        qty--;
+        if (qty < 1) {
+          //console.log(true);
+          $(".popup__content").removeClass("selected");
+          $holder.removeClass("selected");
+        }
+        break;
+    }
+
+    cart[id] = qty;
+    $holder.find(".quantity").text(qty);
+
+    updateCart();
+
+    // console.log(cart);
+    // cart[id] = qty;
+    // $holder.find(".quantity").text(qty);
+    // console.log(cart);
+    // objectCart = Object.assign({}, cart);
+    // console.log(objectCart[id]);
+    // getFood(id);
+
+    // for(var i=0; i<Object.keys(objectCart).length;i++)
+    // updateCart(objectCart[i],id);
+  });
+
+  function updateCart() {
+    const foodList = [];
+    const cart_arr = [];
+    for (let [key, value] of Object.entries(cart)) {
+      console.log(key, value);
+      const food = getFood(key);
+      console.log('ffosd');
+      console.log(food);
+      if(value !==0){
+        foodList.push({
+          id: key,
+          title:food.title, // food.title
+          price: "price", // food.price
+          quantity:value
+        });
+        cart_arr.push(key + ":" + value);
       }
-      break;
+    }
+    const tpl_cart = tmpl($("#template-cart").html());
+    $("#cart").html(tpl_cart({ foodList }));
+    sessionStorage.setItem(CART_NAME, cart_arr.join("-"));
   }
-  cart[id] = qty;
-  $holder.find(".quantity").text(qty);
-});
-  
-//its for show count by click btn in card
-$wrapper.on("click", ".quantity-holder button", e => {
-  e.preventDefault();
-  const $btn = $(e.target).closest("button");
-  const $holder = $btn.parent();
-  const id =$btn.closest(".food_section-category").data("food").id;
-  const title =$btn.closest(".food_section-category").data("food").title;
-  const price =$btn.closest(".food_section-category").data("food").price;
-  let qty = id ? cart[parseInt(id)] || 0 :  0;
-  switch ($btn.data("cmd")) {
-    case "increase":
-      qty++;
-      $holder.addClass("selected") ;
-      break;
-    case "decrease":
-      qty--;
-      if (qty<1){
-        console.log(true);
-        $(".popup__content").removeClass("selected");
-        $holder.removeClass("selected") ;
+  function getFood(key) {
+    //console.log(id);
+    for (var i = 0; i < delinoData.length; i++) {
+      for (var j = 0; j < delinoData[i].sub.length; j++) {
+        if (delinoData[i].sub[j].id == 0) {
+          const subData = delinoData[i].sub[j].food;
+          if (subData) {
+            for (var z = 0; z < subData.length; z++) {
+              // console.log(subData[z].id);
+              if (key === subData[z].id) {
+                console.log(subData[z].title);
+                break;
+              }
+            }
+            // console.log(subData.id);
+          }
+        }
       }
-      break;
-  }
-  console.log(cart);
-  cart[id] = qty;
-  $holder.find(".quantity").text(qty);
-  console.log(cart);
-  objectCart=Object.assign({},cart);
-  console.log(objectCart);
-  for(var i=0; i<Object.keys(objectCart).length;i++)   
-  updateCart(objectCart[i],id);
-});
-function updateCart(objectCart,id){
-  const price =$(".food_section-category").data("food").price;
-  const tpl_updateCart = tmpl($("#template-updateCart").html());
-  // for(var i=0; i<Object.keys(objectCart).length;i++){
-    const selectionItems =tpl_updateCart({
-    orderNumbers:objectCart,
-    foodName:getFood(id),
-    price:calcuteCost(price,objectCart[id])
-    })
-    $('.orders').html(selectionItems);
-  // }
+    }
 }
-function getFood(id){
 
-}
-function calcuteCost(price,count){
-  const newPrice=price*count;
-  return newPrice;
-}
-  // //Add count of food by clicking on btn-plus
-  // $modal.on("click", ".btn-plus", function() {
-  //   const id = $(this)
-  //     .closest(".popup__content")
-  //     .data("data-food-id");
-  //   calcute("add", id);
-  // });
-
-  // //Minus count of food by clicking on btn-minus
-  // $modal.on("click", ".btn-minus", function() {
-  //   calcute("minus", id);
-  // });
-
-  // //calcute for add or minus count of foood
-  // function calcute(type, id) {
-  //   const operation = type === "add" ? (number += 1) : (number -= 1);
-  //   objectCart[id] = number;
-  //   sessionStorage.setItem("cart", JSON.stringify(objectCart));
-  //   // console.log();
-  // }
-
-  //popup
   $categoryWrapper.on("click", "a", function(e) {
     e.preventDefault();
+    G;
     //e.stopPropagation() baes mishe ke event ma edame peida nakone va avalin sath ro begire
     scrolling = true;
     const catId = $(this)
@@ -276,7 +287,9 @@ function calcuteCost(price,count){
 
   //show popup after click on img in it
   $wrapper.on("click", "img", function() {
-    const itemData = $(this).closest(".food_section-category").data("food");
+    const itemData = $(this)
+      .closest(".food_section-category")
+      .data("food");
     renderPopup(itemData);
   });
 
@@ -311,30 +324,3 @@ function calcuteCost(price,count){
       .removeClass("active-box");
   }
 });
-
-
-
-
-// const $box = $(
-//   "#box-" +
-//     $(this)
-//       .closest("div")
-//       .data("cat-id")
-// );
-// $(this)
-//   .closest("div")
-//   .addClass("active-box");
-// $(this)
-//   .closest("div")
-//   .siblings()
-//   .removeClass("active-box");
-// if ($box.length) {
-//   $("body")
-//     .stop()
-//     .animate(
-//       {
-//         scrollTop: $box.offset().top - 100
-//       },
-//       600
-//     );
-// }
